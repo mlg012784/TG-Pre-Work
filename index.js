6e425f43f4df118cc2728fde12b2383c7190a00a
mlg012784 <mlg012784@yahoo.com>
2018-08-20 11:33:39 -0500

    JS 2

--- index.js
+++ index.js
@@ -2,4 +2,10 @@
 console.log("Pepperoni");
 console.log("The Last of Us")
 
+//JS 2                         
+console.log("JavaScript");
+console.log(33.7);
+console.log(true)
+console.log(null)
+
 
