be763381926d33ed1ee9eed1d731b2c8bd8753ca
mlg012784 <mlg012784@yahoo.com>
2018-08-20 12:35:53 -0500

    JS 6

--- index.js
+++ index.js
@@ -20,5 +20,11 @@ console.log("Teaching the world how to code".length);
 //JS 5
 console.log('Codecademy'.toUpperCase()); 
 console.log('    Remove whitespace   '.trim());
-  
+
+//JS 6
+console.log(Math.random() * 100)
+console.log(Math.floor(Math.random() * 100))
+console.log(Math.ceil(43.8))
+console.log(Number.isInteger(2017))
+
  
