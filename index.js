be5305bcd7c43fb3c081eba1e6fd9674072a4b74
mlg012784 <mlg012784@yahoo.com>
2018-08-22 13:45:20 -0500

    JS 50

--- index.js
+++ index.js
@@ -467,6 +467,11 @@ console.log(vacationSpots[0]);
 console.log(vacationSpots[1]);
 console.log(vacationSpots[2]);
 
+//JS 50
+for (let vacationSpotIndex = 0; vacationSpotIndex<vacationSpots.length; vacationSpotIndex++) {
+  console.log('I would love to visit ' + vacationSpots[vacationSpotIndex]);
+}
+
 
 
 
