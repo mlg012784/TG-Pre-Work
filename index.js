41fe9cfaf509013d86fbaaf4f4f926822f120a4c
mlg012784 <mlg012784@yahoo.com>
2018-08-21 22:45:05 -0500

    JS 40

--- index.js
+++ index.js
@@ -352,13 +352,26 @@ visibleLightWaves();
 console.log(lightWaves)
 
 //JS 39
-const visibleLightWaves = () =>{
+const visibleLightWaves = () => {
     let lightWaves = 'Moonlight';
     let region = 'The Arctic'
-    if (region === 'The Arctic'){
+    if (region === 'The Arctic') {
       let lightWaves = 'Northern Lights'
       console.log(lightWaves)
     }
     console.log(lightWaves);
 };
 visibleLightWaves();
+
+
+//JS 40
+const starCount = () => {
+    let i = 5;
+    console.log(i)
+    for (let i = 0; i < 12; i++) {
+      console.log(i)
+    }
+};
+  
+starCount();
+console.log(i)
