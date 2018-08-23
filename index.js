1cb8cf90012f423a9cb25e2a1568be9f82ac5287
mlg012784 <mlg012784@yahoo.com>
2018-08-23 00:20:39 -0500

    JS 80

--- index.js
+++ index.js
@@ -736,8 +736,29 @@ console.log(nurseOlynyk.remainingVacationDays);
 nurseOlynyk.addCertification('Genetics');
 console.log(nurseOlynyk.certifications);
 
-
-
+//JS 80
+class HospitalEmployee {
+  constructor(name) {
+    this._name = name;
+    this._remainingVacationDays = 20;
+  }
+  
+  get name() {
+    return this._name;
+  }
+  
+  get remainingVacationDays() {
+    return this._remainingVacationDays;
+  }
+  
+  takeVacationDays(daysOff) {
+    this._remainingVacationDays -= daysOff;
+  }
+  
+  static generatePassword() {
+    return Math.floor(Math.random() * 10000);
+  }
+}
 
 
 
