347f597d9d923e6faf8cb33380e2b47ee938648a
mlg012784 <mlg012784@yahoo.com>
2018-08-23 18:33:57 -0500

    JS 96

--- index.js
+++ index.js
@@ -937,3 +937,18 @@ import { availableAirplanes, flightRequirements, meetsStaffRequirements} from '.
 
 import meetsSpeedRangeRequirements from './airplane';
 
+//JS 96
+const xhr = new XMLHttpRequest();
+const url = 'https://api-to-call.com/endpoint'
+ 
+xhr.responseType = 'json';
+xhr.onreadystatechange = () => {
+  if (xhr.readyState === XMLHttpRequest.DONE) {
+    return xhr.response;
+  }
+}
+
+xhr.open('GET', url);
+xhr.send();
+
+
