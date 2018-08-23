01a4ee2c7b6909bc1bdf21651297ab35aac7dfc2
mlg012784 <mlg012784@yahoo.com>
2018-08-23 18:55:35 -0500

    JS 100

--- index.js
+++ index.js
@@ -995,3 +995,14 @@ xhr.onreadystatechange = () => {
 xhr.open('POST', url);
 xhr.send(data);
  
+//JS 100
+fetch('https://api-to-call.com/endpoint').then(response => {
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('Request failed!');
+}, networkError => {
+  console.log(networkError.message);
+}).then(jsonResponse => {
+  return jsonResponse;
+});
