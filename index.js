e6a72a3a2577a911821c6d28c60e625cc30ee7fe
mlg012784 <mlg012784@yahoo.com>
2018-08-24 12:15:22 -0500

    JS 104

--- index.js
+++ index.js
@@ -1055,4 +1055,23 @@ const shortenUrl = () => {
   })
 }
 
+//JS 104
+fetch(url, {
+  method: 'POST',
+  headers: {
+    'Content-type': 'application/json',
+    'apikey': apiKey
+  },
+  body: data
+}).then(response => {
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('Request failed!');
+}, networkError => {
+  console.log(networkError.message)
+})
+
+
+
 
