bb3c049088ebccb63d88e83655541dc2ec21bf07
mlg012784 <mlg012784@yahoo.com>
2018-08-23 22:17:13 -0500

    JS 101

--- index.js
+++ index.js
@@ -1006,3 +1006,21 @@ fetch('https://api-to-call.com/endpoint').then(response => {
 }).then(jsonResponse => {
   return jsonResponse;
 });
+
+//JS 101
+const url = 'https://api.datamuse.com/words';
+const queryParams = '?sl=';
+
+
+const getSuggestions = () => {
+const wordQuery = inputField.value;
+const endpoint = `${url}${queryParams}${wordQuery}`;
+
+fetch(endpoint).then(response => {
+  if (response.ok) {
+    return response.json();
+  }
+  throw new Error('Request failed!');
+}, networkError => {
+  console.log(networkError.message)
+}).then(jsonResponse => {renderResponse(jsonResponse)});}
