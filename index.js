e8e205dd5edc1541685308330032f2faee4129a1
mlg012784 <mlg012784@yahoo.com>
2018-08-24 12:26:51 -0500

    JS 109

--- index.js
+++ index.js
@@ -1123,4 +1123,27 @@ const getData = async () => {
   }
 }
 
+//JS 109
+const shortenUrl = async () => {
+	const urlToShorten = inputField.value;
+  const data = JSON.stringify({destination: urlToShorten});
+  try {
+    const response = await fetch(url, {
+			method: 'POST',
+      body: data,
+      headers: {
+        'Content-type': 'application/json',
+				'apikey': apiKey
+      }
+    });
+		if(response.ok){
+      const jsonResponse = await response.json();
+      renderResponse(jsonResponse);
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+
+
 
