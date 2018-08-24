658b855123368804d8d6bffe695763605cff05e7
mlg012784 <mlg012784@yahoo.com>
2018-08-24 12:22:56 -0500

    JS 106

--- index.js
+++ index.js
@@ -1077,6 +1077,20 @@ fetch(url, {
   renderResponse(jsonResponse)
 })
 
+//JS 106
+const getData = async () => {
+  try {
+    const response = await fetch('https://api-to-call.com/endpoint');
+    if (response.ok) {
+      const jsonResponse = await response.json();
+      return jsonResponse;
+    }
+    throw new Error('Request failed!');
+  } catch (error) {
+    console.log(error);	
+  }
+}
+
 
 
 
